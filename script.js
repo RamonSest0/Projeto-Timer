476f7c75901c94473ca9ff2051559d02512fb0fa
Ramon Sesto <ramon.sesto@gmail.com>
2021-11-23 08:38:29 -0300

    corrigindo bugs

--- script.js
+++ script.js
@@ -1,52 +1,67 @@
 
 
-var hours = 0
-var minutes = 0
-var seconds = 0
+var hours = 0;
+var minutes = 0;
+var seconds = 0;
 
-var time = 1000 // 1000 Mílisegundos = 1 segundo
-var timer
+const time = 1000; // 1000 Mílisegundos = 1 segundo
+var timer;
 
 
+function play() {
+    timer = setInterval(playTimer, time)
+};
 
 function start() {
 
-    timer = setInterval(playTimer, time)
+    // if adicionado para corrigir o bug que acelerava o timer toda vez que a função start() era executada.
+    // Basicamente o que acontecia era que toda vez que start() era chamado um setInterval era executado,
+    // então caso start() fosse executado mais de uma vez ficavam vários setInterval() sendo executados ao mesmo tempo.
+    // O bug foi resolvido usando clearTimeout() para resetar o setTimeout() ativo, assim, se start for executado mais de uma vez o setTimeout() ativo...
+    // será resetado, e logo em seguida será executado um novo setTimeout(). F bug :D
 
-}
+    if (seconds == 0) {
+        play();
+    }
+    else {
+        clearTimeout(timer);
+        play();
+    }
+
+};
 
 function stop() {
 
-    clearInterval(timer)
+    clearInterval(timer);
 
-}
+};
 
 function reset() {
 
-    clearInterval(timer)
-    hours = 0
-    minutes = 0
-    seconds = 0
+    clearInterval(timer);
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
 
-    document.getElementById('display').innerHTML = '00:00:00'
+    document.getElementById('display').innerHTML = '00:00:00';
 
 }
 
 function playTimer() {
 
-    seconds++
+    seconds += 1;
 
     // Sempre que chegar a 60 segundos incrementa 1 minuto
     if (seconds == 60) {
         seconds = 0
         minutes++
-    }
+    };
 
     // Sempre que chegar a 60 minutos incrementa uma hora
     if (minutes == 60) {
         minutes = 0
         hours++
-    }
+    };
 
     // Para sempre ficar com dois dígitos
     var format = (hours < 10 ? '0' + hours : hours)
@@ -56,7 +71,7 @@ function playTimer() {
         (seconds < 10 ? '0' + seconds : seconds)
     document.getElementById('display').innerHTML = format
 
-}
+};
 
 
 
